a3e14bf32f6bc6b34b6af0c692835c63e1f8593f
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-04-25 02:51:53 +0430

    update models

--- funTrips/MVP/models.py
+++ funTrips/MVP/models.py
@@ -17,13 +17,24 @@ class Profile(models.Model):
     sex = models.CharField(max_length=6,choices=gender_choices)
 
 class Destination (models.Model):
-    address = models.TextField(max_length=500)
+    name = models.CharField(max_length=100,default='place_name')
+    address = models.TextField(max_length=500,default='plcae_add')
+    phone = models.IntegerField(default='09123456789')
+
 
 class Entertainment (models.Model):
+    type_choices = (
+        ('Food','Food'),
+        ('Exciting','Exciting'),
+        ('Sport','Sport'),
+        ('Relaxtion','Relaxtion'),
+    )
+
+    place = models.ForeignKey(Destination,on_delete=models.CASCADE,default='9999')
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
+    type = models.CharField(choices=type_choices,max_length=100)
     duration = models.TimeField()
-    price = models.DecimalField(max_digits=10,decimal_places=3)
+    price = models.DecimalField(max_digits=12,decimal_places=3)
 
 class Order (models.Model):
     client = models.ForeignKey(Profile,on_delete=models.CASCADE)
