f48561e40ff276595295259b804dc7519c59820a
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-04-25 02:06:15 +0430

    create models

--- funTrips/funTrips/MVP/models.py
+++ funTrips/funTrips/MVP/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# User (Profile)
+#Destination(Places)
+#Entertainment(Actions)
+
+class Profile(models.Model):
+    client = models.ForeignKey(User,on_delete=models.CASCADE)
+    gender_choices = (
+        ('M','Male'),
+        ('F','Female')
+    )
+    name = models.CharField(max_length=100)
+    age =  models.IntegerField()
+    sex = models.CharField(choices=gender_choices)
+
+class Destination (models.Model):
+    address = models.TextField(max_length=500)
+
+class Entertainment (models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    duration = models.TimeField()
+    price = models.DecimalField()
+
+class order (models.Model):
+    client = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    place =  models.ForeignKey(Destination,on_delete=models.CASCADE)
+    activity = models.ForeignKey(Entertainment,on_delete=models.CASCADE)
+    price = client = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    duration = client = models.ForeignKey(Profile,on_delete=models.CASCADE)
